3551d37dae6f7c27d1ceed1d4a7ade39f906cd5f
Dishan Gimhan <kdgimhan@gmail.com>
2025-05-23 18:02:09 +0530

    login

--- FE/src/app/user-login/user-login.component.ts
+++ FE/src/app/user-login/user-login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../user-create/user-create.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router'; // <-- Import Router
+
 
 @Component({
   selector: 'app-login-user',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
@@ -16,7 +19,7 @@ export class UserLoginComponent {
   });
   message = '';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {} // <-- Inject Router
 
   loginUser() {
     if (this.loginForm.valid) {
@@ -24,6 +27,7 @@ export class UserLoginComponent {
       this.userService.login(email!, password!).subscribe(
         (res) => {
           this.message = res.message;
+          this.router.navigate(['']); // <-- Navigate on success
         },
         (error) => {
           this.message = error.error?.message || 'Login failed.';
